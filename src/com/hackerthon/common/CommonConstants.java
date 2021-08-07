6444d35bda9a8fdcafc6350e0f6508aeba9c8251
supun-priyashan <68734943+supun-priyashan@users.noreply.github.com>
2021-08-07 15:51:27 +0530

    gerfgwertghweth

--- src/com/hackerthon/common/CommonConstants.java
+++ src/com/hackerthon/common/CommonConstants.java
@@ -48,22 +48,5 @@ public class CommonConstants {
     /** Constant for driver key of MySQL database in config.properties */
     public static final String DRIVER_NAME = "driverName";
 
-    /** Constant for query id of drop_table in EmployeeQuery.xml */
-    public static final String QUERY_ID_DROP_TABLE = "delete_employee" ;
-
-    /** Constant for query id of create_table in EmployeeQuery.xml */
-    public static final String QUERY_ID_CREATE_TABLE = "create_employee_table";
-
-    /** Constant for query id of insert_employees in EmployeeQuery.xml */
-    public static final String QUERY_ID_INSERT_EMPLOYEES = "insert_employee";
-
-    /** Constant for query id of get_employee_byId in EmployeeQuery.xml */
-    public static final String QUERY_ID_GET_EMPLOYEE_BYID = "get_employee_ById";
-
-    /** Constant for query id of get_all_employees in EmployeeQuery.xml */
-    public static final String QUERY_ID_GET_ALL_EMPLOYEES = "get_employees";
-
-    /** Constant for delete_employee in EmployeeQuery.xml */
-    public static final String DELETE_EMPLOYEE = "get_employees";
 
 }
